811eab35db5ea67bdbb439db67484118d3e8c826
dat0913416955 <quocdat10102002@gmail.com>
2022-12-15 14:07:02 +0700

    hoan tat do án

--- CSharpNet7/QuestionMark/Program.cs
+++ CSharpNet7/QuestionMark/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+
+int number = 10;
+double money = 10.5;
+bool check = true;
+string str = "nguyen";
+
+//Trong C Sharp thường có 2 dạng kiểu chính
+// 1.value type (dạng số):không chứa dc null
+// 2.reference type (chuỗi,đối tượng):cho phép null
+
+string name = null;
+//Code Cũ
+Nullable<bool> a = null;
+
+//Code Mới Dấu ? : => nullable value type
+int? b= null;
+bool? flag = null;
+
+
+//if(name != null)
+//{
+//    Console.WriteLine(true);
+//}
+//else
+//{
+//    Console.WriteLine(false);
+//}
+
+//?: => ternary operator
+Console.WriteLine(name != null ? true : false);
+// is not  (!=)
+// is (==)
+Console.WriteLine(name is not null ? true : false);
+
+//int age = 10;
+//if (age is 10)
+//{
+
+//}
+
+//     ? => null conditional operator
+//     ? dùng sau đít tên biến , hay đối tượng
+//     ? tương tự (!=) ,nếu cái gì đó khác null thì lấy cái vế sau
+int? length = name?.Length;
+/*
+if (length != null)
+{
+    length=name.Length;
+}
+else
+{
+    length = null;
+}*/
+
+//??=> null coalescring operator
+//?? tương tự dấu == ,nếu cái gì đó bằng null thì lấy cái vế sau
+
+int? chieudai = name?.Length ?? 10;
